eb4407cc6a6f3bfd5d79de5f46093d59f6fe7c6e
joelsteven <joelofelectronics@gmail.com>
2024-04-02 02:11:09 +0300

    Fix: app layout responsiveness

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -8,6 +8,7 @@ import { FcCurrencyExchange } from "react-icons/fc";
 import { FcSettings } from "react-icons/fc";
 import { FcNext } from "react-icons/fc";
 import { FcPrevious } from "react-icons/fc";
+import { FcMenu } from "react-icons/fc";
 import {
   Layout,
   Menu,
@@ -16,6 +17,9 @@ import {
   Typography,
   Avatar,
   Breadcrumb,
+  Grid,
+  Drawer,
+  Space,
 } from "antd";
 import { useRouter, usePathname } from "next/navigation";
 import ProfileAvatar from "@/components/header/ProfileAvatar";
@@ -29,9 +33,87 @@ type AppLayoutProps = {
   activeMenu?: string;
 };
 
+const { useBreakpoint } = Grid;
+
+const MenuComponent: React.FC<{
+  items: {
+    key: string;
+    icon: React.ReactNode;
+    label: string;
+    onClick: () => void;
+  }[];
+
+  activeMenu?: string;
+}> = ({ items, activeMenu }) => {
+  return (
+    <Menu
+      theme="light"
+      mode="inline"
+      defaultSelectedKeys={["1"]}
+      selectedKeys={[activeMenu || "1"]}
+    >
+      {items.map((item) => (
+        <Menu.Item
+          key={item.key}
+          icon={item.icon}
+          onClick={item.onClick}
+          style={{ cursor: "pointer" }}
+        >
+          {item.label}
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+};
+
 const AppLayout: React.FC<AppLayoutProps> = ({ children, activeMenu }) => {
   const router = useRouter();
   const pathname = usePathname();
+
+  const screens = useBreakpoint();
+
+  const menuItems = [
+    {
+      key: "1",
+      icon: <FcBarChart />,
+      label: "Dashboard",
+      onClick: () => {
+        router.push("/dashboard", { scroll: false });
+      },
+    },
+    {
+      key: "2",
+      icon: <FcMoneyTransfer />,
+      label: "Transactions",
+      onClick: () => {
+        router.push("/transactions", { scroll: false });
+      },
+    },
+    {
+      key: "3",
+      icon: <FcBriefcase />,
+      label: "Portfolio",
+      onClick: () => {
+        router.push("/portfolio", { scroll: false });
+      },
+    },
+    {
+      key: "4",
+      icon: <FcCurrencyExchange />,
+      label: "Cryptocurrencies",
+      onClick: () => {
+        router.push("/cryptocurrencies", { scroll: false });
+      },
+    },
+    {
+      key: "5",
+      icon: <FcSettings />,
+      label: "Settings",
+      onClick: () => {
+        router.push("/settings", { scroll: false });
+      },
+    },
+  ];
 
   // function that takes in pathname, removes the starting "/" and capitalizes the first letter
   function formatPathname(pathname: string) {
@@ -44,101 +126,95 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children, activeMenu }) => {
   } = theme.useToken();
 
   return (
-    <div style={{ height: "100vh" }} className="flex">
+    <div style={{ height: "100vh", width: "100vw" }} className="flex">
       <Layout>
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={collapsed}
-          style={{ background: colorBgContainer, maxHeight: "100%" }}
-        >
-          <div
-            className="demo-logo-vertical"
+        {screens.xs ? (
+          <Drawer
+            title="Cryptallion"
+            placement="left"
+            closable={false}
+            onClose={() => setCollapsed(false)}
+            open={collapsed}
+            key="left"
             style={{
-              display: "flex",
-              justifyContent: "center",
-              padding: "16px",
+              background: colorBgContainer,
+              padding: 0,
             }}
-            onClick={() => router.push("/", { scroll: false })}
           >
-            <Image
-              src="/images/logo.png" // Route of the image file
-              height={100} // Desired size with correct aspect ratio
-              width={100} // Desired size with correct aspect ratio
-              alt="Cryptallion Logo"
-              style={{ cursor: "pointer" }}
-              priority
-            />
-          </div>
-          <Menu
-            theme="light"
-            mode="inline"
-            defaultSelectedKeys={["1"]}
-            selectedKeys={[activeMenu || "1"]}
-            items={[
-              {
-                key: "1",
-                icon: <FcBarChart />,
-                label: "Dashboard",
-                onClick: () => {
-                  router.push("/dashboard", { scroll: false });
-                },
-              },
-              {
-                key: "2",
-                icon: <FcMoneyTransfer />,
-                label: "Transactions",
-                onClick: () => {
-                  router.push("/transactions", { scroll: false });
-                },
-              },
-              {
-                key: "3",
-                icon: <FcBriefcase />,
-                label: "Portfolio",
-                onClick: () => {
-                  router.push("/portfolio", { scroll: false });
-                },
-              },
-              {
-                key: "4",
-                icon: <FcCurrencyExchange />,
-                label: "Cryptocurrencies",
-                onClick: () => {
-                  router.push("/cryptocurrencies", { scroll: false });
-                },
-              },
-              {
-                key: "5",
-                icon: <FcSettings />,
-                label: "Settings",
-                onClick: () => {
-                  router.push("/settings", { scroll: false });
-                },
-              },
-            ]}
-          />
-        </Sider>
+            <MenuComponent items={menuItems} activeMenu={activeMenu} />
+          </Drawer>
+        ) : (
+          <Sider
+            trigger={null}
+            collapsible
+            collapsed={collapsed}
+            style={{
+              background: colorBgContainer,
+            }}
+          >
+            <div
+              className="demo-logo-vertical"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                padding: "16px",
+              }}
+              onClick={() => router.push("/", { scroll: false })}
+            >
+              <Image
+                src="/images/logo.png" // Route of the image file
+                height={100} // Desired size with correct aspect ratio
+                width={100} // Desired size with correct aspect ratio
+                alt="Cryptallion Logo"
+                style={{ cursor: "pointer" }}
+                priority
+              />
+            </div>
+            <MenuComponent items={menuItems} activeMenu={activeMenu} />
+          </Sider>
+        )}
         <Layout>
           <Header
             style={{
-              padding: 5,
               background: colorBgContainer,
               display: "flex",
               justifyContent: "space-between",
               alignItems: "center",
             }}
           >
-            <Button
-              type="text"
-              icon={collapsed ? <FcNext /> : <FcPrevious />}
-              onClick={() => setCollapsed(!collapsed)}
-              style={{
-                fontSize: "16px",
-                width: 64,
-                height: 64,
-              }}
-            />
+            {
+              // if screens.xs is true, show the FcMenu icon, else show the Cryptallion logo
+              screens.xs ? (
+                <Space>
+                  <Button
+                    type="text"
+                    icon={<FcMenu />}
+                    onClick={() => setCollapsed(!collapsed)}
+                    className="cursor-pointer"
+                    size="large"
+                  />
+                  <Avatar
+                    src="/images/logo.png"
+                    // size="large"
+                    size={60}
+                    style={{ cursor: "pointer" }}
+                    onClick={() => router.push("/", { scroll: false })}
+                  />
+                </Space>
+              ) : (
+                <Button
+                  type="text"
+                  icon={collapsed ? <FcNext /> : <FcPrevious />}
+                  onClick={() => setCollapsed(!collapsed)}
+                  style={{
+                    fontSize: "16px",
+                    width: 64,
+                    height: 64,
+                  }}
+                  className="cursor-pointer"
+                />
+              )
+            }
             <ProfileAvatar notifications={5} />
           </Header>
           <Breadcrumb style={{ margin: "16px", cursor: "pointer" }}>
@@ -159,19 +235,14 @@ const AppLayout: React.FC<AppLayoutProps> = ({ children, activeMenu }) => {
               background: colorBgContainer,
               borderRadius: borderRadiusLG,
               overflow: "auto",
+              maxWidth: "95vw",
             }}
           >
-            {/* centered div with activeMenu title */}
             <Title level={2} style={{ textAlign: "center" }}>
               {formatPathname(pathname)}
             </Title>
             {children}
           </Content>
-          {/* <Footer style={{ textAlign: "center" }}>
-            <Title level={5}>
-              Cryptallion ©2024 Created by Cryptallion Team
-            </Title>
-          </Footer> */}
         </Layout>
       </Layout>
     </div>
