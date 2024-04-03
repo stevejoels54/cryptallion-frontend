bf7365a44525299c429b1f77e9670c67bc223131
joelsteven <joelofelectronics@gmail.com>
2024-04-03 15:57:04 +0300

    Update page content and add button for authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,34 @@
+"use client";
+
 import Image from "next/image";
+import { Button } from "antd";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Image
-        src="/logo.svg"
+        src="/images/logo.png"
         alt="Logo"
         className="w-80"
         width={300}
         height={300}
       />
       <h1 className="text-6xl font-bold">Welcome to cryptallion</h1>
-      <p className="mt-3 text-2xl">Get started by editing</p>
+      <p className="mt-3 text-2xl">
+        A platform for tracking your cryptocurrency transactions
+      </p>
+      <div className="mt-4">
+        <Button
+          type="primary"
+          size="large"
+          onClick={() => router.push("/auth", { scroll: false })}
+        >
+          Get Started
+        </Button>
+      </div>
     </div>
   );
 }
